8dfacc19d480ba9459d6cec571e168f92c32bb7d
yuchun <xx830921xx@gmail.com>
2021-02-26 09:15:34 +0800

    NOW rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,16 @@
+import random
+
+
 def main():
-    print('You rolled a die')
+
+    dice_sum = 0
+    dice_rolls = 2
+    for i in range(dice_rolls):
+      roll = random.randint(1, 6)
+      dice_sum += roll
+      print(f'You rolled a {roll}')  # 格式化字符串字面值
+
+    print(f'You have rolled a total of {dice_sum}')
 
 
 if __name__ == "__main__":
